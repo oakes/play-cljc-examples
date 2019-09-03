2dd7628333cf044258615012c261de0bcac02759
oakes <zach@sekao.net>
2019-09-03 07:54:53 -0400

    Remove nightlight

--- dungeon-crawler/src/dungeon_crawler/start_dev.cljs
+++ dungeon-crawler/src/dungeon_crawler/start_dev.cljs
@@ -1,6 +1,5 @@
 (ns dungeon-crawler.start-dev
   (:require [dungeon-crawler.start]
-            [nightlight.repl-server]
             [orchestra-cljs.spec.test :as st]
             [expound.alpha :as expound]
             [clojure.spec.alpha :as s]))
--- super-koalio/src/super_koalio/start_dev.cljs
+++ super-koalio/src/super_koalio/start_dev.cljs
@@ -1,6 +1,5 @@
 (ns super-koalio.start-dev
   (:require [super-koalio.start]
-            [nightlight.repl-server]
             [orchestra-cljs.spec.test :as st]
             [expound.alpha :as expound]
             [clojure.spec.alpha :as s]))
